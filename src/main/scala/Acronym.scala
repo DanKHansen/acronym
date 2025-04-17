ba6a63d5d62be8c204d9f01464d46372c609fa1d
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-04-17 10:05:09 +0200

    refaction

--- src/main/scala/Acronym.scala
+++ src/main/scala/Acronym.scala
@@ -1,2 +1,3 @@
 object Acronym:
-   def abbreviate(phrase: String): String = phrase.split("[ ,-]+").map(_.head.toUpper).mkString
+   def abbreviate(phrase: String): String =
+      phrase.split("[ ,-]+").map(_.head.toUpper).mkString
